f97da1bd9cadc6f30cfa151fd535f765c1964e6d
Shockang <shockang@163.com>
2022-02-10 20:00:37 +0800

    leetcode901_1000

--- src/main/java/com/shockang/study/algorithm/java/leetcode/leetcode901_1000/leetcode968/solution1/Solution.java
+++ src/main/java/com/shockang/study/algorithm/java/leetcode/leetcode901_1000/leetcode968/solution1/Solution.java
@@ -18,8 +18,11 @@ public class Solution {
 		int[] l = dfs(root.left);
 		int[] r = dfs(root.right);
 		int[] array = new int[3];
+		// root 有监控，整棵树被覆盖，这时只需要保证 2 个子树的所有子树都被覆盖
 		array[0] = l[2] + r[2] + 1;
+		// 整棵树被覆盖，root 有没有监控无所谓，左孩子有监控然后右子树被覆盖或者右孩子有监控左子树被覆盖
 		array[1] = Math.min(array[0], Math.min(l[0] + r[1], r[0] + l[1]));
+		// 2 个子树被覆盖，root 有没有被覆盖无所谓
 		array[2] = Math.min(array[0], l[1] + r[1]);
 		return array;
 	}
